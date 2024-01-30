495080443a493fbdda4365c068ab0c8cea380183
Diego Florença <diego.florenca@gmail.com>
2024-01-29 22:14:52 -0800

    bubble sort

--- sketch.js
+++ sketch.js
@@ -1,8 +1,42 @@
+const bars = [5, 4, 7, 3, 9, 1, 6, 2, 8];
+let sorted = false;
+let x = 20;
+let y = 180;
+let selectedIndex = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  frameRate(10);
 }
 
 function draw() {
   background(150);
-  rect(180, 180, 40, 40);
+  rectMode(CENTER);
+  for (let index = 0; index < bars.length; index++) {
+    const element = bars[index] * 10;
+    if (index === selectedIndex && !sorted) {
+      fill(0);
+    } else if (index === selectedIndex + 1 && !sorted) {
+      fill(155);
+    } else {
+      fill(255);
+    }
+    rect(x, y, 25, element);
+    x += 30;
+  }
+  x = 20;
+
+  if (bars[selectedIndex] > bars[selectedIndex + 1]) {
+    const temp = bars[selectedIndex];
+    bars[selectedIndex] = bars[selectedIndex + 1];
+    bars[selectedIndex + 1] = temp;
+    sorted = false;
+  } else {
+    sorted = true;
+  }
+
+  selectedIndex++;
+  if (selectedIndex > bars.length) {
+    selectedIndex = 0;
+  }
 }
